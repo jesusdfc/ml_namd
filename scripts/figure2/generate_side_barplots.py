dca7317f5ae1ccfe95dd00f131d9a4eee94b8edc
ubioinformat <ubioinformat@unav.es>
2023-03-03 13:06:37 -0500

    barplot generated sorted by median

--- scripts/figure2/generate_side_barplots.py
+++ scripts/figure2/generate_side_barplots.py
@@ -12,13 +12,19 @@ df = pd.read_csv(os.path.join('inputs','mean_rAUC_per_variables_and_models.tsv')
 Iterate one by one
 """
 
-color_palette = ['#5FCAAA','#FF8964','#77A1CD']
+color_palette = ['#5FCAAA','#77A1CD','#FF8964']
 
 for var in ['Atrophy_36m','Fibrosis_36m','Atrophy|Fibrosis_36m']:
 
     # creating subplots
     ax = plt.subplots()
-    ax = sns.barplot(data=df[df['experiment'] == var], x="Variables", y="rAUROC", hue= 'Method', palette=color_palette)
+    subdf = df[df['experiment'] == var]
+    #generate sum of groups
+    groupsum_dict = subdf.groupby('Variables').median().to_dict()
+    groupsum = [groupsum_dict['rAUROC'][x] for x in subdf['Variables']]
+    subdf['groupsum'] = groupsum
+    ssubdf = subdf.sort_values(by='groupsum',ascending=False)
+    ax = sns.barplot(data= ssubdf, x= "Variables", y= "rAUROC", hue= 'Method', palette= color_palette, hue_order=['rf','xgboost','svm'])
     
     ax.set_yticklabels(labels=[-0.025, 0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14])
     plt.yticks([-0.025, 0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14])
