6d2d6901636f7b743880243d50268812736fb1f8
ubioinformat <ubioinformat@unav.es>
2023-03-02 19:11:40 -0500

    included grouped rauc barplot

--- scripts/figure2/generate_side_barplots.py
+++ scripts/figure2/generate_side_barplots.py
@@ -3,47 +3,29 @@ import os
 from matplotlib import pyplot as plt
 import seaborn as sns
 import numpy as np
+import matplotlib.patches as mpatches
 plt.rcParams['pdf.fonttype'] = 42
 
-df = pd.read_csv(os.path.join('inputs','mean_rAUC_per_group_of_features.tsv'),sep='\t')
-df.columns = ['Group'] + df.columns[1:].values.tolist()
-df = pd.melt(df,id_vars=['Group'], value_vars=['Atrophy_36m|Fibrosis_36m','Fibrosis_36m','Atrophy_36m'])
-df.columns = ['Group','Disease','Mean rAUC']
-
-##define groupal order
-order_group = df.groupby('Group')['Mean rAUC'].aggregate(np.sum).reset_index().sort_values('Mean rAUC', ascending=False)
-
+df = pd.read_csv(os.path.join('inputs','mean_rAUC_per_variables_and_models.tsv'),sep='\t',index_col=0)
 
 """
 Iterate one by one
 """
 
-var_dict = {'Atrophy_36m|Fibrosis_36m':0, 'Fibrosis_36m':1, 'Atrophy_36m':2}
-color_palette = ['#917C62','#3175A2','#E1812C']
+color_palette = ['#5FCAAA','#77A1CD','#FF8964']
 
-var = 'Fibrosis_36m'
-#read df
-df = pd.read_csv(os.path.join('inputs','mean_rAUC_per_group_of_features.tsv'),sep='\t')
-df = df.iloc[:,[0,var_dict[var]+1]]
-df.columns = ['Group'] + df.columns[1:].values.tolist()
-df = pd.melt(df,id_vars=['Group'], value_vars=['Atrophy_36m|Fibrosis_36m','Fibrosis_36m','Atrophy_36m'][var_dict[var]])
-df.columns = ['Group','Disease','Mean rAUC']
+for var in ['Atrophy_36m','Fibrosis_36m','Atrophy|Fibrosis_36m']:
 
+    # creating subplots
+    ax = plt.subplots()
+ 
+    ax = sns.barplot(data=df[df['experiment'] == 'Atrophy_36m'], x="Variables", y="rAUROC", ci=None, color= color_palette[0])
+    ax = sns.barplot(data=df[df['experiment'] == 'Fibrosis_36m'], x="Variables", y="rAUROC", ci=None, color= color_palette[1])
+    ax = sns.barplot(data=df[df['experiment'] == 'Atrophy|Fibrosis_36m'], x="Variables", y="rAUROC", ci=None, color= color_palette[2])
 
-
-fig=plt.figure(figsize=(15,10))
-ax = sns.barplot(data=df, x="Group", y="Mean rAUC", order = order_group['Group'],
-                 palette = [color_palette[var_dict[var]]])
-
-
-# ax.legend(labels=("Female", "Male"),)
-ax.set_ylim([-0.01,0.14])
-# ax.set_xticklabels(labels=[-100, -50, -25, 0, 50, 100])
-ax.set_yticklabels(labels=[-0.01, 0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14])
-plt.yticks([-0.01, 0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14])
-
-
-plt.savefig(os.path.join('figures',f"{var.replace('|','_')}_mean_rAUC_per_group_of_features.pdf"))  
+    ax.set_yticklabels(labels=[-0.025, 0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14])
+    plt.yticks([-0.025, 0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14])
+    plt.savefig(os.path.join('figures','figure2',f"{var.replace('|','_')}_mean_rAUC_per_group_of_features.pdf"))  
 
 
 
